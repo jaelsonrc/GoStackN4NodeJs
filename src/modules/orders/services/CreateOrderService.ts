e3a6e0790e1fe69d10c11eca338c1e01a93b7096
Jaelson R Cunha <jaelsonrc@hotmail.com>
2020-11-29 20:11:52 +0100

    Desafio 09: Relacionamentos com banco de dados no Node.js

--- src/modules/orders/services/CreateOrderService.ts
+++ src/modules/orders/services/CreateOrderService.ts
@@ -20,13 +20,64 @@ interface IRequest {
 @injectable()
 class CreateOrderService {
   constructor(
+    @inject('OrdersRepository')
     private ordersRepository: IOrdersRepository,
+    @inject('ProductsRepository')
     private productsRepository: IProductsRepository,
+    @inject('CustomersRepository')
     private customersRepository: ICustomersRepository,
   ) {}
 
   public async execute({ customer_id, products }: IRequest): Promise<Order> {
-    // TODO
+    const customerOrm = await this.customersRepository.findById(customer_id);
+
+    if (!customerOrm) throw new AppError('O cliente não existe!');
+
+    const productsOrm = await this.productsRepository.findAllById(products);
+
+    if (!productsOrm.length) throw new AppError('Produtos não encontrados');
+
+    const productsIds = productsOrm.map(product => product.id);
+
+    const checkNoIds = products.filter(
+      product => !productsIds.includes(product.id),
+    );
+
+    if (checkNoIds.length) throw new AppError('Produtos não encontrados');
+
+    const findProductsWithNoQuantityAvailable = products.filter(
+      product =>
+        productsOrm.filter(p => p.id === product.id)[0].quantity <
+        product.quantity,
+    );
+
+    if (findProductsWithNoQuantityAvailable.length) {
+      throw new AppError('Quantidade invalido');
+    }
+
+    const serializedProducts = products.map(product => ({
+      product_id: product.id,
+      quantity: product.quantity,
+      price: productsOrm.filter(p => p.id === product.id)[0].price,
+    }));
+
+    const order = await this.ordersRepository.create({
+      customer: customerOrm,
+      products: serializedProducts,
+    });
+
+    const { order_products } = order;
+
+    const orderProductsQuantity = order_products.map(product => ({
+      id: product.product_id,
+      quantity:
+        productsOrm.filter(p => p.id === product.product_id)[0].quantity -
+        product.quantity,
+    }));
+
+    await this.productsRepository.updateQuantity(orderProductsQuantity);
+
+    return order;
   }
 }
 
